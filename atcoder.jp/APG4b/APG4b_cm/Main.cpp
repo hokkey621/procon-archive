58aed0e607334f50216d2cbd07176c4e120420a5
hokki <ibukiz621@yahoo.co.jp>
2020-08-16 14:18:27 +0000

    [AC] APG4b APG4b_cm

---
+++ atcoder.jp/APG4b/APG4b_cm/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int i;
+  //Aの出力
+  cout<<"A:";
+  for(i=0;i<A;i++){
+  cout<<']';
+  }
+  cout<<endl;
+  //Bの出力
+  cout<<"B:";
+  for(i=0;i<B;i++){
+  cout<<']';
+  }
+  cout<<endl;
+  
+  return 0;
+}
