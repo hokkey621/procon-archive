0323bfd295e92333fe356e92aadba776ff4834b0
hokki <ibukiz621@yahoo.co.jp>
2020-08-16 13:49:02 +0000

    [AC] APG4b APG4b_cn

---
+++ atcoder.jp/APG4b/APG4b_cn/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  // 1.の出力
+  x++;
+  cout << x << endl;
+
+  // ここにプログラムを追記
+  //2の出力
+  int x2=x*(a+b);
+  cout<<x2<<'\n';
+  //3の出力
+  int x3=x2*x2;
+  cout<<x3<<'\n';
+  //4の出力
+  x3--;
+  cout<<x3<<'\n';
+  
+  
+}
