728175f8c9798091ff20f330165c76d85a1d5f64
hokki <ibukiz621@yahoo.co.jp>
2020-12-15 13:37:57 +0000

    [AC] abc171 abc171_a

---
+++ atcoder.jp/abc171/abc171_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+  char s;
+  cin>>s;
+  
+  if('a'<=s && s<='z')cout<<'a'<<endl;
+  else cout<<'A'<<endl;
+  return 0;
+  
+}
