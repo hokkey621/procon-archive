5944a9093c7aa97538732c05910ba9670cba5bc9
hokki <ibukiz621@yahoo.co.jp>
2021-03-13 12:01:19 +0000

    [AC] abc195 abc195_a

---
+++ atcoder.jp/abc195/abc195_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void){
+    int m, h;
+    cin >> m >> h;
+    if(h % m == 0)cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
