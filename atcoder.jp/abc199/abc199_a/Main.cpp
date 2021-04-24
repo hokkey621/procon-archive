b72f746035a59fb2d1e3bef500738e4a029df533
hokki <ibukiz621@yahoo.co.jp>
2021-04-24 12:01:35 +0000

    [AC] abc199 abc199_a

---
+++ atcoder.jp/abc199/abc199_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a * a + b * b < c * c) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
