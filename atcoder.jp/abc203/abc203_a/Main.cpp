5fe0bd2ac2c1dadc5da55816f098df77d5bc45a1
hokki <ibukiz621@yahoo.co.jp>
2021-05-30 12:01:51 +0000

    [AC] abc203 abc203_a

---
+++ atcoder.jp/abc203/abc203_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+    int a, b, c;
+    cin >> a >> b >>c;
+    if(a == b) cout << c << endl;
+    else if(b == c) cout << a << endl;
+    else if(c == a) cout << b << endl;
+    else cout << 0 << endl;
+    return 0;
+}
