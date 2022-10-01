03183144fad0f68c603675c85453c458a42be111
hokki <ibukiz621@yahoo.co.jp>
2022-10-01 12:06:27 +0000

    [AC] abc271 abc271_a

---
+++ atcoder.jp/abc271/abc271_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+const ll MOD = 1000000007;
+
+
+int main(void) {
+    int n;
+    cin >> n;
+    printf("%02X\n", n);
+}
