8b64e68a78a537bcc4487737d2c544e4d14318d9
hokki <ibukiz621@yahoo.co.jp>
2022-10-22 12:03:22 +0000

    [AC] abc274 abc274_a

---
+++ atcoder.jp/abc274/abc274_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+const ll MOD = 1000000007;
+
+
+int main(void) {
+    double a, b;
+    cin >> a >> b;
+    printf("%.3lf", b / a);
+    return 0;
+}
