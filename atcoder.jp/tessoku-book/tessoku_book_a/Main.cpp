4c67a2a1416a959af030a857ac4477e1f6cd8858
hokki <ibukiz621@yahoo.co.jp>
2023-04-10 11:29:08 +0000

    [AC] tessoku-book tessoku_book_a

---
+++ atcoder.jp/tessoku-book/tessoku_book_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+const ll MOD = 1000000007;
+
+
+int main(void) {
+    int N;
+    cin >> N;
+    cout << N * N << endl;
+    return 0;
+}
