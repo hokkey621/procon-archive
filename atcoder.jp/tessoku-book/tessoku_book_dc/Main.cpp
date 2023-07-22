3a46b84f33a0321f58cdae5908c68be0a12de9a2
hokki <ibukiz621@yahoo.co.jp>
2023-07-22 08:47:49 +0000

    [AC] tessoku-book tessoku_book_dc

---
+++ atcoder.jp/tessoku-book/tessoku_book_dc/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for(int i = 0; i < (n); ++i)
+const ll MOD = 1000000007;
+
+
+ll mypower(ll a, ll b) {
+    ll p = a;
+    ll ans = 1;
+    rep(i, 30) {
+        ll div = (1 << i);
+        if((b / div) % 2 == 1) {
+            ans = (ans * p) % MOD;
+        }
+        p = (p * p) % MOD;
+    }
+    return ans;
+}
+
+
+int main(void) {
+    // 入力
+    ll H, W;
+    cin >> H >> W;
+    --H;
+    --W;
+
+    //分子aを求める
+    ll a = 1;
+    for(ll i = 1; i <= (W + H); ++i) a = a * i % MOD;
+
+    //分母bを求める
+    ll b = 1;
+    for(ll i = 1; i <= W; ++i) b = b * i % MOD;
+    for(ll i = 1; i <= H; ++i) b = b * i % MOD;
+
+    //出力
+    cout << (a * mypower(b, MOD - 2) % MOD) << endl;
+    return 0;
+}
